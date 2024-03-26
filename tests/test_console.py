7a2131cbede524b7582ed9570926a360be0188de
JustGodWork <discord1elie@gmail.com>
2024-03-26 17:37:19 +0100

    Added tests for do_create

---
+++ tests/test_console.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+    Unittest for console class
+"""
+
+
+import unittest
+from console import HBNBCommand
+from unittest.mock import patch
+from io import StringIO
+
+
+class TestHBNBCommand(unittest.TestCase):
+    """
+    Test cases for the console
+    """
+    def setUp(self):
+        self.cli = HBNBCommand()
+
+    def test_do_create_no_args(self):
+        """
+        Test create command with no arguments
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.cli.onecmd("create")
+            self.assertEqual(f.getvalue(), "** class name missing **\n")
+
+    def test_do_create_invalid_class(self):
+        """
+        Test create command with invalid class
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.cli.onecmd("create MyClass")
+            self.assertEqual(f.getvalue(), "** class doesn't exist **\n")
+
+    def test_do_create_valid_class_no_params(self):
+        """
+        Test create command with valid class and no params
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.cli.onecmd("create BaseModel")
+            self.assertNotEqual(f.getvalue(), "** class doesn't exist **\n")
+            self.assertNotEqual(f.getvalue(), "** class name missing **\n")
+
+    def test_do_create_valid_class_with_params(self):
+        """
+        Test create command with valid class and params
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.cli.onecmd("create BaseModel name=\"John\"")
+            self.assertNotEqual(f.getvalue(), "** class doesn't exist **\n")
+            self.assertNotEqual(f.getvalue(), "** class name missing **\n")
+
+    def test_do_create_valid_class_with_invalid_params(self):
+        """
+        Test create command with valid class and invalid params
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.cli.onecmd("create BaseModel name")
+            self.assertNotEqual(f.getvalue(), "** class doesn't exist **\n")
+            self.assertNotEqual(f.getvalue(), "** class name missing **\n")
+
+
+if __name__ == '__main__':
+    unittest.main()
