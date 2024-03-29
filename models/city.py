1bc759a26a7cc845c60db1611fd89cd747ab863c
JustGodWork <discord1elie@gmail.com>
2024-03-29 10:41:17 +0100

    Fixed State and City classes behavior

--- models/city.py
+++ models/city.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from os import getenv
+from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel, Base):
-    """ City class """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class City(BaseModel, Base):
+        """ City class """
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all")
+else:
+    class City(BaseModel):
+        """ City class """
+        state_id = ""
+        name = ""
--- models/state.py
+++ models/state.py
@@ -8,17 +8,41 @@ from models.city import City
 from os import getenv
 
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class State(BaseModel, Base):
+        """ State class """
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete", backref="state")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+        if getenv("HBNB_TYPE_STORAGE") != "db":
+            @property
+            def cities(self):
+                """
+                    Getter attribute cities that returns the
+                    list of City instances
+                    with state_id equals to the current State.id
+                """
+                from models import storage
+                city_list = []
+                for city in storage.all(City).values():
+                    if city.state_id == self.id:
+                        city_list.append(city)
+                return city_list
+
+else:
+    class State(BaseModel):
+        """ State class """
+        name = ""
+        cities = {}
+
         @property
         def cities(self):
-            """Getter attribute cities that returns the list of City instances
-            with state_id equals to the current State.id"""
+            """
+                Getter attribute cities that returns
+                the list of City instances
+                with state_id equals to the current State.id
+            """
             from models import storage
             city_list = []
             for city in storage.all(City).values():
