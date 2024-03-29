318a5d7191167522feff04ee2e291060a85096cf
JustGodWork <discord1elie@gmail.com>
2024-03-29 18:19:49 +0100

    Added test class for db storage

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module for testing file storage"""
+from .test_file_storage import test_fileStorage
+
+
+class test_dbStorage(test_fileStorage):
+	""" Class to test the file storage method """
+
+	def setUp(self):
+		""" Set up test environment """
+		pass
