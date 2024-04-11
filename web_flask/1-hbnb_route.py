d435119dd7d4e2710da22c6af531b0f69f562213
JustGodWork <discord1elie@gmail.com>
2024-04-11 11:47:21 +0200

    Added task 2 (Web Framework)

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Create Flask App"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def listen():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def listen_hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False)
