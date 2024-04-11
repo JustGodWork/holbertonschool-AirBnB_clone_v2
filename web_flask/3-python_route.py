cb78871cdf4d1b3ed248047d1c89844c786fa020
JustGodWork <discord1elie@gmail.com>
2024-04-11 11:58:47 +0200

    Added task 4 (Web Framework)

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Create Flask App"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def listen():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def listen_hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def listen_c(text):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={'text': "is_cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def listen_python(text="is cool"):
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False)
